99d9f1540187a660b9e8f12c688fa76cd32cbe91
DonTheChimera <donaldtbradfield@gmail.com>
2019-03-13 08:55:47 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PomodoroComponent } from './pomodoro/pomodoro.component';
+
+const routes: Routes = [
+  { path: '', component: PomodoroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pomodoro/pomodoro.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pomodoro',
+  templateUrl: './pomodoro.component.html',
+  styleUrls: ['./pomodoro.component.css']
+})
+export class PomodoroComponent implements OnInit {
+  keepingTime: boolean = false;
+
+  timerMinutes: number = 25;
+
+  timerStarted: boolean = false;
+
+  timerMessageStart: string = "Start your timer!";
+
+  timerMessageStop: string = "Stop your timer!";
+
+  revealAbout: boolean = false;
+
+  timer = setInterval(() => {
+    if (this.timerStarted && this.timerMinutes !== 0) this.timerMinutes--;
+    if (this.timerMinutes === 0) {
+      clearInterval(this.timer);
+      alert('Time expired!');
+    }
+  }, 1000);
+
+
+  timerMode() {
+    this.keepingTime = !this.keepingTime;
+    this.timerStarted = !this.timerStarted;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onStartTimer() {
+    this.timer;
+    this.timerMode();
+  }
+
+  onResetTimer() {
+    confirm("You sure you wanna reset?");
+    if (this.keepingTime) {
+      this.timerMode();
+      this.timerMinutes = 25;
+    } else {
+      this.timerMinutes = 25;
+    }
+  }
+
+}
